5186ca138cc127d5b84249e54bad6cccb50d1aae
j-vargas6 <68479847+j-vargas6@users.noreply.github.com>
2020-12-09 09:15:28 -0600

    Update streamlit-visualizations.py

--- streamlit-visualizations.py
+++ streamlit-visualizations.py
@@ -8,10 +8,10 @@ import matplotlib.pyplot as plt
 import re
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
-#import seaborn as sns
+import seaborn as sns
 import numpy as np
 from datetime import datetime 
-#import plotly.express as px
+import plotly.express as px
 from itertools import cycle
 
 ####DATA#####
@@ -23,6 +23,33 @@ df4 = pd.read_csv('acres_merged_prediction_results.csv')
 df5 = pd.read_csv('predict_raw.zip')
 
 ####DATA TRANSFORMATION / DATA QUERIES#####
+@st.cache
+def load():
+ 	df = pd.read_csv('fire_data.csv')
+ 	df['YEAR2'] = pd.to_datetime(df['YEAR'],format='%Y')
+ 	df['STARTDATED'] = pd.to_datetime(df['STARTDATED'],errors='coerce')
+ 	df['CONTRDATED'] = pd.to_datetime(df['CONTRDATED'],errors='coerce')
+ 	df['Days'] = (df['CONTRDATED'] - df['STARTDATED']).dt.days
+ 	return df
+
+@st.cache
+def load2():
+	df = pd.read_excel('Data.xlsx',3)
+	df['cost_per_acre'] = (df['Estimated Cost']/df['Acres'])
+	df['Start Date'] = pd.to_datetime(df['Start Date'], errors='coerce')
+	df['Contain or Last Report Date'] = pd.to_datetime(df['Contain or Last Report Date'],errors='coerce')
+	df['Days'] = (df['Contain or Last Report Date'] - df['Start Date']).dt.days
+	df = df[(df['Days'] > 0) & (df['cost_per_acre'] > 0)]
+	df = df[(df['Cause'] == 'L') | (df['Cause'] == 'H')]
+	df['Cause'].replace({'H':'Human','L':'Natural'}, inplace=True)
+	return df
+
+@st.cache
+def load3():
+	df = pd.read_excel('Data.xlsx')
+	df['cost_per_acre'] = df['Total']/df['Acres']
+
+	return df
 
 #Data processing for the Cost Predictor Model
 #Not to be used for visualizations
@@ -86,57 +113,7 @@ def get_bigData_cause(df5):
     result = df5[['CAUSE', 'YEAR_', 'STATE', 'FIRETYPE', 'PROTECTION', 'TOTALACRES']]
     return result
 
-# def predictor_viz(df4, df2):
-#     st.header('3. Insert Section Header Here')
-#     st.write('3.1 Instructions:...')
-
-#     a_result = df4[['Year', 'Point_Forecast', 'Model']]
-#     cost_df = df2[['Year', 'Acres', 'Estimated_Cost']]
-#     year_df = cost_df[cost_df['Year'] == 2019].dropna()
-#     acre_ave = sum(year_df['Acres'])
-#     cost_ave = sum(year_df['Estimated_Cost'])
-#     cost_per_acre = cost_ave/acre_ave
-#     a_result['Cost_per_Acre'] = a_result['Point_Forecast'] * cost_per_acre
-
-#     pred = alt.Chart(a_result).mark_point().encode(
-#     alt.X('Year:O', scale=alt.Scale(zero=False)),
-#     alt.Y('Point_Forecast:Q', scale=alt.Scale(zero=False)),
-#     color=alt.Color('Model:N', legend = None),
-#     tooltip = [alt.Tooltip('Model:N'), alt.Tooltip('Point_Forecast:Q', title = 'Future Acres Burned')]
-#     ).properties(width = 600, height = 400)
-    
-#     text = pred.mark_text(align = 'left', baseline = 'middle', dx = 7).encode(
-#     text = 'Model'
-#     )
-
-#     secondpred = alt.Chart(a_result).mark_point().encode(
-#     alt.X('Year:O', scale=alt.Scale(zero=False)),
-#     alt.Y('Cost_per_Acre:Q', scale=alt.Scale(zero=False)),
-#     color=alt.Color('Model:N', legend = None),
-#     tooltip = [alt.Tooltip('Model:N'), alt.Tooltip('Cost_per_Acre:Q', title = 'Future Cost'),
-#                 alt.Tooltip('Year:O')]
-#     ).properties(width = 600, height = 400)
-    
-    
-#     secondtext = secondpred.mark_text(align = 'left', baseline = 'middle', dx = 7).encode(
-#     text = 'Model'
-#     )
-
-#     #charts
-#     pred + text
-#     secondpred + secondtext
-#     st.subheader('3.2 Dataset...')
-#     st.write('something')
-#     st.subheader('3.3 Relevance')
-#     st.write('something...importance of viz')
-#     st.subheader('3.4 Prediction Methodology')
-#     st.write('Summary of methods')
-#     st.subheader('3.5 Prediction Constraints and Limitations')
-#     st.write('Summary of cons and limits')
-#     st.subheader('3.6 Summary')
-#     st.write('Summary of findings')
-
-def human_v_natural(df5):
+def human_v_natural():
     result = get_bigData_cause(df5)
     result = result.groupby(['YEAR_','STATE', 'CAUSE']).size().reset_index(name="cause count")
 
@@ -159,7 +136,7 @@ def human_v_natural(df5):
     st.subheader('1.4 Summary')
     st.write('Summary of findings')
 
-def other_viz(df5):
+def other_viz():
     result = get_bigData_cause(df5)
     st.header('2. Insert Section Header Here')
     st.subheader('2.1 Instructions')
@@ -217,7 +194,7 @@ def other_viz(df5):
 
     fire_type + text1 | protect + text2
 
-def other_viz2(df5):
+def other_viz2():
     result = get_bigData_cause(df5)
 
     Selector_year = list(result['YEAR_'].sort_values().unique())
@@ -274,6 +251,40 @@ def other_viz2(df5):
     st.write('2.2.2 Definition of protection levels')
     st.subheader('2.3 Summary')
     st.write('Summary of findings')
+
+def other_viz3():
+    result = get_bigData_cause(df5)
+
+    types = ['Action Fires/Supressed Fires', 'Natural Out', 'Support Action/Assist Fire', 
+                'Fire Management/Perscribed', 'False Alarm', 'Severe']
+
+    firetype_df2 = result.groupby(['YEAR_','FIRETYPE']).size().reset_index(name="Firetype Count")
+    firetype_df2['FIRETYPE'] = firetype_df2['FIRETYPE'].replace(0, 'Action Fires/Supressed Fires')
+    firetype_df2['FIRETYPE'] = firetype_df2['FIRETYPE'].replace(1, 'Natural Out')
+    firetype_df2['FIRETYPE'] = firetype_df2['FIRETYPE'].replace(2, 'Support Action/Assist Fire')
+    firetype_df2['FIRETYPE'] = firetype_df2['FIRETYPE'].replace(3, 'Fire Management/Perscribed')
+    firetype_df2['FIRETYPE'] = firetype_df2['FIRETYPE'].replace(4, 'False Alarm')
+    firetype_df2['FIRETYPE'] = firetype_df2['FIRETYPE'].replace(5, 'Severe')
+    
+    base = alt.Chart(firetype_df2, width=600, height=600).mark_point(filled=True).encode(
+    x=alt.X('YEAR_:N', axis=alt.Axis(labelAngle=45), title = 'Year'),
+    y='Firetype Count:Q',
+    tooltip="FIRETYPE:N"
+    )
+
+    type_radio = alt.binding_radio(options=types)
+    type_select = alt.selection_single(fields=['FIRETYPE'], bind=type_radio)
+    type_color_condition = alt.condition(type_select,
+                      alt.Color('FIRETYPE:N', legend=None),
+                      alt.value('lightgray'))
+    highlight_types = base.add_selection(
+    type_select
+    ).encode(
+    color=type_color_condition
+    ).properties(title="Fill In")
+
+    highlight_types
+
 
 def display_pred_code():
     #big data acrage predictor
@@ -372,7 +383,183 @@ def render_predictions():
         x, y = 0, 0
     return x, y 
 
+def sammys_viz():
+        df = load()
+        df_sig = load2()
+        df_main = load3()
 
+        states = alt.topo_feature(data.us_10m.url, 'states')
+        source = 'https://raw.githubusercontent.com/sammyhajomar/test/main/altair-dataset1.csv'
+        variables = ['State','acres','id']
+
+        # US states background
+        background = alt.Chart(states).mark_geoshape(
+            fill='lightgray',
+            stroke='white'
+        ).properties(
+            title='Wild Fires by State'
+        ).project('albersUsa'
+        ).properties(
+                    width=800,
+                    height=500
+        )
+
+        slider = alt.binding_range(min=1980, max=2016, step=1, name='Year')
+
+        select_year = alt.selection_single(name="SelectorName", fields=['YEAR'],
+                                        bind=slider, init={'YEAR': 2000})
+
+        single = alt.selection_single(empty='all', fields=['CAUSE'])
+
+        fire_points = alt.Chart(df).mark_circle(  
+            ).encode(
+                size=alt.Size("TOTALACRES:Q", title='Acres Burned', scale=alt.Scale(range=[0, 1000])),
+                longitude='DLONGITUDE:Q',
+                latitude='DLATITUDE:Q',
+                tooltip=[alt.Tooltip('YEAR',title='Year'),alt.Tooltip('TOTALACRES',title='Acres Burned', format = ",.4r"), 
+                            alt.Tooltip('CAUSE',title='Cause'),alt.Tooltip('STARTDATED',title='Start Date'),alt.Tooltip('STATE',title='State')],
+                color='CAUSE'
+            ).add_selection(
+                select_year
+            ).transform_filter(
+                select_year
+        )
+
+        chart_1 = alt.Chart(df).mark_line(point=True).encode(
+                alt.X('year(YEAR2):T',title='Year',scale=alt.Scale(zero=False),axis=alt.Axis(titleFontSize=14,labelFontSize=14)),
+                alt.Y('sum(TOTALACRES):Q',title='Total Acres Burned', scale=alt.Scale(zero=False), axis=alt.Axis(titleFontSize=14,labelFontSize=14)),
+                color='CAUSE:N',
+                tooltip=[alt.Tooltip("YEAR2",title='Year'), alt.Tooltip('mean(TOTALACRES)',title='Acres Burned', format=",.4r")]
+            ).properties(
+                width=800,
+                height=300
+        )
+
+        chart_2 = alt.Chart(df).mark_line(point=True).encode(
+                alt.X('year(YEAR2):T',title='Year',scale=alt.Scale(zero=False),axis=alt.Axis(titleFontSize=14,labelFontSize=14)),
+                alt.Y('sum(Days):Q',title='Total Number of Days per Fires', scale=alt.Scale(zero=False), axis=alt.Axis(titleFontSize=14,labelFontSize=14)),
+                color='CAUSE:N',
+                tooltip=[alt.Tooltip("YEAR2",title='Year'), alt.Tooltip('mean(Days)',title='Days of Fire', format=".2r")]
+            ).properties(
+                width=800,
+                height=300
+        )
+
+        chart_3 = alt.Chart(df).mark_bar().encode(
+                alt.X('STATE',title='State', sort='-y'),
+                alt.Y('sum(TOTALACRES):Q',title='Acres Burned', axis=alt.Axis(format=',.4r')),
+                color=alt.Color('CAUSE:N'),
+                tooltip=[alt.Tooltip("CAUSE",title='Cause'), alt.Tooltip('sum(TOTALACRES)',title='Acres Burned', format=",.4r")]
+            ).add_selection(
+                select_year
+            ).transform_filter(
+                select_year
+        )
+
+        US_map = alt.Chart(states).mark_geoshape().encode(
+                color=alt.Color('acres:Q',title='Acreage Burned'),
+                tooltip=['State:N',alt.Tooltip('acres:Q',title='Total Acres Burned', format = ",.4r")]
+            ).transform_lookup(
+                lookup='id',
+                from_=alt.LookupData(source,'id',variables),
+            ).project(
+                type='albersUsa'
+        )
+
+        brush = alt.selection(type='interval', encodings=['x'])
+
+        bar1 = alt.Chart(df_main).mark_bar().encode(
+            x = alt.X('Year:O', scale=alt.Scale(zero=False),axis=alt.Axis(titleFontSize=20,labelFontSize=14)),
+            y = alt.Y('Total:Q', scale=alt.Scale(zero=False), title='Total Cost', axis=alt.Axis(titleFontSize=20,labelFontSize=14, format = "$,.6r")),
+            opacity=alt.condition(brush, alt.OpacityValue(1), alt.OpacityValue(0.7)),
+            tooltip=[alt.Tooltip("Total:Q",title='Total Cost', format="$,.4r"), alt.Tooltip("Year",title='Year')]
+        ).add_selection(
+            brush
+        )
+
+        rule1 = alt.Chart(df_main).mark_rule(color='red').encode(
+                y='mean(Total):Q',
+                size=alt.SizeValue(3)
+            ).transform_filter(
+                brush
+        )
+
+        bar2 = alt.Chart(df_main).mark_bar().encode(
+            x = alt.X('Year:O', scale=alt.Scale(zero=False),axis=alt.Axis(titleFontSize=20,labelFontSize=14)),
+            y = alt.Y('Acres:Q', scale=alt.Scale(zero=False), title='Acres Burned', axis=alt.Axis(titleFontSize=20,labelFontSize=14)),
+            opacity=alt.condition(brush, alt.OpacityValue(1), alt.OpacityValue(0.7)),
+            tooltip=[alt.Tooltip("Acres",title='Acres Burned', format=",.6r"), alt.Tooltip("Year",title='Year')]
+        ).add_selection(
+            brush
+        )
+
+        rule2 = alt.Chart(df_main).mark_rule(color='red').encode(
+            y='mean(Acres):Q',
+            size=alt.SizeValue(3)
+        ).transform_filter(
+            brush
+        )
+
+        bar3 = alt.Chart(df_main).mark_bar().encode(
+            x = alt.X('Year:O', scale=alt.Scale(zero=False),axis=alt.Axis(titleFontSize=20,labelFontSize=14)),
+            y = alt.Y('Fires:Q', scale=alt.Scale(zero=False), title='Number of Fires', axis=alt.Axis(titleFontSize=20,labelFontSize=14)),
+            opacity=alt.condition(brush, alt.OpacityValue(1), alt.OpacityValue(0.7)),
+            tooltip=[alt.Tooltip("Fires",title='Number of Fires', format=",.4r"),alt.Tooltip("Year",title='Year')]
+        ).add_selection(
+            brush
+        )
+
+        rule3 = alt.Chart(df_main).mark_rule(color='red').encode(
+            y='mean(Fires):Q',
+            size=alt.SizeValue(3)
+        ).transform_filter(
+            brush
+        )
+
+        bar4 = alt.Chart(df_main).mark_bar().encode(
+            x = alt.X('Year:O', scale=alt.Scale(zero=False),axis=alt.Axis(titleFontSize=20,labelFontSize=14)),
+            y = alt.Y('cost_per_acre:Q', scale=alt.Scale(zero=False), title='Cost per Acre', axis=alt.Axis(titleFontSize=20,labelFontSize=14, format = "$,.2r")),
+            opacity=alt.condition(brush, alt.OpacityValue(1), alt.OpacityValue(0.7)),
+            tooltip=[alt.Tooltip("cost_per_acre",title='Cost per Acre', format="$,.2r"),alt.Tooltip("Year",title='Year')]
+        ).add_selection(
+            brush
+        )
+
+        rule4 = alt.Chart(df_main).mark_rule(color='red').encode(
+            y='mean(cost_per_acre):Q',
+            size=alt.SizeValue(3)
+        ).transform_filter(
+            brush
+        )
+
+        slider1 = alt.binding_range(min=2006, max=2019, step=1, name='Year')
+
+        select_year1 = alt.selection_single(name="SelectorName", fields=['Year'],
+                                        bind=slider1, init={'Year': 2006})
+
+        scatter = alt.Chart(df_sig).mark_circle(size=200
+            ).encode(
+                x=alt.X('Days:Q', title='Days of Fire',scale=alt.Scale(zero=False),axis=alt.Axis(titleFontSize=20,labelFontSize=14)),
+                y=alt.Y('cost_per_acre:Q', title='Cost per Acre', scale=alt.Scale(zero=False),axis=alt.Axis(titleFontSize=20,labelFontSize=14,format="$,.2r")),
+                color= alt.Color('Cause:N'),
+                tooltip=[alt.Tooltip('Name:N',title='Name of Fire'), alt.Tooltip('Acres:Q',title='Acres Burned'), alt.Tooltip('cost_per_acre:Q',title="Cost per Acre", format="$,.2r"),alt.Tooltip('Days:Q',title="Days of Fire")],
+                size=alt.Size("Acres:Q", title='Acres Burned',scale=alt.Scale(range=[0, 1000])),
+            ).properties(
+                width=800,
+                height=500
+            ).add_selection(
+                select_year1
+            ).transform_filter(
+                select_year1
+        )
+
+        scatter
+        (bar1 + rule1) & (bar2 + rule2) & (bar3 + rule3) & (bar4 + rule4)
+        map_chart = background + fire_points
+        map_chart
+        chart_3
+        chart_1
+        chart_2
 
 if __name__ == "__main__":
     st.set_page_config(layout="wide")
@@ -458,9 +645,10 @@ if __name__ == "__main__":
             causePlots(year)
 
     elif(add_selectbox == 'Analysis'):
-        human_v_natural(df5)
-        other_viz(df5)
-        other_viz2(df5)
+        sammys_viz()
+        other_viz()
+        other_viz2()
+        other_viz3()
 
     elif(add_selectbox == 'Predictions'):
         st.title('Time Series Forecasting: Future Acres Burned and Furure Cost of Suppression')
@@ -483,10 +671,4 @@ if __name__ == "__main__":
         predictPlot(df, flag, num)
         predictCost(df, flag, num)
         display_pred_code()
-       
-
-
-
-
-
-
+   
